3e270340daad44373501dff0e3a7dcc45827d9ba
Dhirendra kumar <dhirendra985dhiru@gmail.com>
2020-07-29 15:30:24 +0530

    Updates

---
+++ precache-manifest.5785ceae13d6a37f4f362ba71ab97b2a.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69d95a9f8b945fd0a55048a34c2fd7c6",
+    "url": "/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "0cb05a7d069a628ac1fd",
+    "url": "/COVID-19-Tracker/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "0e860aa46b00e6fd3095",
+    "url": "/COVID-19-Tracker/static/css/main.3cfdca17.chunk.css"
+  },
+  {
+    "revision": "0cb05a7d069a628ac1fd",
+    "url": "/COVID-19-Tracker/static/js/2.6b3d0683.chunk.js"
+  },
+  {
+    "revision": "f6cbbee24959228a1f4c27cfb61712fb",
+    "url": "/COVID-19-Tracker/static/js/2.6b3d0683.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e860aa46b00e6fd3095",
+    "url": "/COVID-19-Tracker/static/js/main.931ce8b9.chunk.js"
+  },
+  {
+    "revision": "7fffeb50ad7c339dc0d9",
+    "url": "/COVID-19-Tracker/static/js/runtime-main.ffc7bb6c.js"
+  },
+  {
+    "revision": "7d2a0e9745ed70847a5ce6d30f96e813",
+    "url": "/COVID-19-Tracker/static/media/aboutcorona.7d2a0e97.jpg"
+  },
+  {
+    "revision": "23671bdfcd47e92da86f81d7c4dbd46a",
+    "url": "/COVID-19-Tracker/static/media/breath.23671bdf.png"
+  },
+  {
+    "revision": "4d4d2c3a5080952d4d354b208f4f77fa",
+    "url": "/COVID-19-Tracker/static/media/cough.4d4d2c3a.png"
+  },
+  {
+    "revision": "61db2bcc402908b39a0925fd6ee0a64c",
+    "url": "/COVID-19-Tracker/static/media/eksath.61db2bcc.png"
+  },
+  {
+    "revision": "7d15924c431d60cff50de685a60af738",
+    "url": "/COVID-19-Tracker/static/media/fever.7d15924c.png"
+  },
+  {
+    "revision": "c8fdf0c0622288dbbde48b4037c3f911",
+    "url": "/COVID-19-Tracker/static/media/handwash.c8fdf0c0.png"
+  },
+  {
+    "revision": "9e2188e642849c7ab3c7f17ef3229be7",
+    "url": "/COVID-19-Tracker/static/media/home.9e2188e6.png"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19-Tracker/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "d1784f496058a52c51ecd995dd46c157",
+    "url": "/COVID-19-Tracker/static/media/logo1.d1784f49.jpg"
+  },
+  {
+    "revision": "e49fffc20da999640c8adecc9f3583a9",
+    "url": "/COVID-19-Tracker/static/media/mask.e49fffc2.png"
+  },
+  {
+    "revision": "a544368c91c35d272efd7f3b74ee09c3",
+    "url": "/COVID-19-Tracker/static/media/news.a544368c.png"
+  },
+  {
+    "revision": "05d9c057ae9d97dde786c2224cafeac0",
+    "url": "/COVID-19-Tracker/static/media/quarantine.05d9c057.png"
+  },
+  {
+    "revision": "c0ec4881fc5a96cd0c711ebd2ca99def",
+    "url": "/COVID-19-Tracker/static/media/sick.c0ec4881.png"
+  },
+  {
+    "revision": "8a5c6cb06d43b0befe28cc9f1c54fa6d",
+    "url": "/COVID-19-Tracker/static/media/week.8a5c6cb0.png"
+  }
+]);
